6c263fd6016a1b50058cb96634cb747cdb85f2da
Pepe Valdivia <jvaldivia@softweb.pe>
2018-01-04 18:53:30 -0500

    Commit inciial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sockets Lua
+
+Instlación de Turbo:
+
+    $ sudo apt-get install lua5.1 luajit luarocks git build-essential libssl-dev
+    $ sudo luarocks install turbo
+
+### Fuentes
+
++ http://turbo.readthedocs.io/en/latest/get_started.html
---
+++ app.lua
@@ -0,0 +1,17 @@
+-- Import turbo,
+local turbo = require("turbo")
+
+-- Create a new requesthandler with a method get() for HTTP GET.
+local HelloWorldHandler = class("HelloWorldHandler", turbo.web.RequestHandler)
+function HelloWorldHandler:get()
+    self:write("Hello World!")
+end
+
+-- Create an Application object and bind our HelloWorldHandler to the route '/hello'.
+local app = turbo.web.Application:new({
+    {"/hello", HelloWorldHandler}
+})
+
+-- Set the server to listen on port 8888 and start the ioloop.
+app:listen(8888)
+turbo.ioloop.instance():start()
