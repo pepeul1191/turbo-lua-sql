04c421f76d5002387903210d40f7ea2573d0291d
Pepe Valdivia <jvaldivia@softweb.pe>
2018-01-04 19:12:52 -0500

    Agregando archivo de configuración constants

--- handlers/home.lua
+++ handlers/home.lua
@@ -1,9 +1,10 @@
 local turbo = require("turbo")
+local constants = require("config.constants")
 local M = {}
 
 local Index = class("Index", turbo.web.RequestHandler)
 function Index:get()
-  self:write("home")
+  self:write("home <br> " .. constants.BASE_URL)
 end
 
 M.Index = Index
